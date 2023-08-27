b15f7ce35164664191362e5cf9527040f59ac0f3
Manas8274 <manas9425658116@gmail.com>
2023-08-27 14:33:37 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 50;
+        int b = 10;
+        int c = 30;
+
+        int num = (a>b)? (a>c? a: c): (b>c? b:c);
+        System.out.println("Larges Number is :" + num);
+
+        }
+    }
